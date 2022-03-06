328bcf4b8e2d9ed562b0af74d12ac24fde5b1fbf
MikolaiSib <MikolaiSib@gmail.com>
2022-03-06 15:00:22 +0700

    Add ClassComp, fix Users FuncComp -> ClassComp, 54 lesson

--- my-app/src/components/Users/Users.tsx
+++ my-app/src/components/Users/Users.tsx
@@ -3,21 +3,25 @@ import {UsersPropsType} from "./UsersContainer";
 import s from "./users.module.css"
 import axios from "axios";//import  * as axios from "axios"
 import avatar from "../../image/allAva.jpg"
+import App from "../../App";
 
-export const Users = (props: UsersPropsType) => {
+class Users extends React.Component<any, any> {
 
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                props.setUsers(response.data.items)
+    // constructor(props: any) {
+    //     super(props);
+    // }
+
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                this.props.setUsers(response.data.items)
             })
-        }
     }
 
-    return (
-        <div>
-            <button onClick={getUsers}>get users</button>
-            {props.users.map(u => <div key={u.id}>
+    render() {
+        return (
+            <div>
+                {this.props.users.map((u: any) => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small != null ? u.photos.small : avatar} alt="No ava"
@@ -26,14 +30,14 @@ export const Users = (props: UsersPropsType) => {
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-                                props.unfollow(u.id)
+                                this.props.unfollow(u.id)
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(u.id)
+                                this.props.follow(u.id)
                             }}>Follow</button>}
                     </div>
                 </span>
-                <span>
+                    <span>
                     <span>
                         <div>
                             {u.name}
@@ -42,17 +46,19 @@ export const Users = (props: UsersPropsType) => {
                             {u.status}
                         </div>
                     </span>
-                    {/*<span>*/}
-                    {/*    <div>*/}
-                    {/*        {u.location.country}*/}
-                    {/*    </div>*/}
-                    {/*    <div>*/}
-                    {/*        {u.location.city}*/}
-                    {/*    </div>*/}
-                    {/*</span>*/}
+                        {/*<span>*/}
+                        {/*    <div>*/}
+                        {/*        {u.location.country}*/}
+                        {/*    </div>*/}
+                        {/*    <div>*/}
+                        {/*        {u.location.city}*/}
+                        {/*    </div>*/}
+                        {/*</span>*/}
                 </span>
-            </div>)}
-        </div>
-    );
-};
+                </div>)}
+            </div>
+        );
+    }
+}
 
+export default Users;
